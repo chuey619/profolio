7ee0833771f96d44a80adfd7520be575f63f2b1f
chueyi619 <chuey.israel@gmail.com>
2020-10-14 13:08:39 -0700

    mobile stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = (props) => {
       <li  onClick={()=> scrollInto(props.aboutRef)} className="nav-item">About</li>
       <li onClick={()=> scrollInto(props.projectsRef)} className="nav-item">Projects</li>
       <li onClick={()=> scrollInto(props.contactRef)} className="nav-item">Contact</li>
-      <li className='nav-item'><a target='_blank' href='./resume.pdf'>Resume</a></li>
+      <li className='nav-item'><a target='_blank' href='https://drive.google.com/file/d/1gGCBmfhvS_De4MmEBC217k2Z1hm9Z4KI/view?usp=sharing'>Resume</a></li>
     </ul>
   );
 };
